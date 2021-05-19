99fdd048a20e1ff6c7e194aad3bb6c5aa59bf573
Eduardo guadalupe Romero quiñonez <eduardogrq@Eduardos-iMac.local>
2021-05-19 16:58:27 -0500

    Se realiza la conexión con la base de datos de MongoDB, se crea esquema para koders y filtros por nombre

---
+++ index.js
@@ -0,0 +1,56 @@
+
+// DB Conection
+const mongoose = require('mongoose');
+const DB_USER = 'eduardogrq'
+const DB_PASSWORD = 'Tuyyosomos1mismo'
+const DB_HOST = 'kodemia11g.utnt4.mongodb.net'
+const DB_NAME = 'kodemia'
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+
+//Schema
+
+const KoderSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minLenght: 2,
+        maxLenght: 100,
+        required: true
+    },
+    lastName: {
+        type: String,
+        minLenght: 2,
+        maxLenght: 100,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 0,
+        max: 99,
+        require: true
+    },
+    gender: {
+        type: String,
+        maxLenght: 1,
+        required: true,
+        enum:['m', 'f'],
+        required: true
+    }
+})
+
+const Koder = mongoose.model('koders', KoderSchema)
+
+ mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then((conn) => {
+        console.log('DB Connection :D ', conn)
+        Koder.find({ name: "Mariana" })
+            .then((kodersFound) => {
+                console.log('Koders encontrados: ', kodersFound)
+            })
+            .catch((error) => {
+                console.log('error: ', error)
+            })
+    })
+    .catch((error) => {
+        console.log('error: ', error)
+    })
